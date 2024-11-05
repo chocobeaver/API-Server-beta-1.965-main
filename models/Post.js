810109f18cb612178338f7e6a120012dbaaf36a5
chocobeaver <106833828+chocobeaver@users.noreply.github.com>
2024-11-05 15:53:40 -0500

    init

---
+++ models/Post.js
@@ -0,0 +1,17 @@
+import Model from './model.js';
+
+export default class Post extends Model {
+    constructor() {
+        super(true /* secured Id */);
+
+  
+        this.addField('Title', 'string');
+        this.addField('Text', 'string');
+        this.addField('Category', 'string');
+        this.addField('Image', 'asset');
+        this.addField('Creation', 'integer');
+        this.setKey("Id");
+    }
+
+    
+}
