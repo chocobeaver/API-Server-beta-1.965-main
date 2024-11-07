f397b33d4111478cd8fc2c68ca43f5f930606595
ancario <anthonylabbe68@gmail.com>
2024-11-07 10:50:27 -0500

    c

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,9 +1,10 @@
-import PostModel from '../models/post.js';
+// import PostModel from '../models/post.js';
+import BookmarkModel from '../models/bookmark.js';
 import Repository from '../models/repository.js';
 import Controller from './Controller.js';
 
 export default class PostsController extends Controller {
     constructor(HttpContext) {
-        super(HttpContext, new Repository(new PostModel()));
+        super(HttpContext, new Repository(new BookmarkModel()));
     }
 }
