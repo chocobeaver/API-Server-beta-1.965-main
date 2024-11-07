42471c0562f69b1270401d62f85cdc0066072d85
ancario <anthonylabbe68@gmail.com>
2024-11-07 10:43:04 -0500

    c

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -2,7 +2,7 @@ import PostModel from '../models/post.js';
 import Repository from '../models/repository.js';
 import Controller from './Controller.js';
 
-export default class ContactsController extends Controller {
+export default class PostsController extends Controller {
     constructor(HttpContext) {
         super(HttpContext, new Repository(new PostModel()));
     }
